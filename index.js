c63cd49bd7fa62f054bbb8e3d323df32463d32c9
WeiLi Fan <whales@WeiLis-MacBook-Pro.local>
2018-04-25 10:39:31 +0800

    refactor: swtich to threejs

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // import * as PIXI from "pixi.js";
 // import { TweenMax } from 'gsap';
 // import PixiPlugin from "gsap/PixiPlugin";
-import {
-  setup,
-  transitionToNumber
-} from './src/process.js'
+// import {
+//   setup,
+//   transitionToNumber
+// } from './src/process.js'
 
-// set up draw board
-setup()
+// // set up draw board
+// setup()
 
-// after some delay, prepare the transitioning to number
-setTimeout(() => {
-  transitionToNumber();
-}, 0)
+// // after some delay, prepare the transitioning to number
+// setTimeout(() => {
+//   transitionToNumber();
+// }, 0)
